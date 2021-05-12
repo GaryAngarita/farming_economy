d035630b6aec16caaa82708963dff2add2d35f18
GaryAngarita <gary.angarita@gmail.com>
2021-05-12 16:14:26 -0400

    add models
    add user creation and start validation

--- farm_app/models.py
+++ farm_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+#class UserManager(models.Manager):
+#    def basic_validator(self, postData)
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    username = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+#    objects = UserManager()
+
 # Create your models here.
